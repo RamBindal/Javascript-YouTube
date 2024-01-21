6e0c669abf4c2e5c0cf45b66981473ad8b7086fb
RamBindal <128307816+RamBindal@users.noreply.github.com>
2024-01-21 17:27:08 +0000

    Dates

---
+++ 01_basics/08_dates-in-js.js
@@ -0,0 +1,24 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(typeof(myDate))
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toJSON());
+
+// let myTimeStamp = Date.now()              //Miliseconds
+// console.log(myTimeStamp);
+// console.log(Math.floor(myTimeStamp/1000))
+
+// let seconds=1705857273                    //Seconds
+// console.log(Math.floor(seconds/86400));   //Days
+
+// let newDate = Date.now()
+// console.log(newDate);
+newDate.toLocaleString('default',{
+    weekday:"long",
+})
+console.log(newDate);
