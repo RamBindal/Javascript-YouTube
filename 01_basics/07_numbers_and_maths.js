552ca571043fcfbca5b2c05391c420751a0d62c3
RamBindal <128307816+RamBindal@users.noreply.github.com>
2024-01-21 16:18:32 +0000

    number-maths

---
+++ 01_basics/07_numbers_and_maths.js
@@ -0,0 +1,30 @@
+///////++++++++++++++++++ NUMBERS ++++++++++++++/////////////////////
+//const score = 200
+//const balance = new Number(200)
+//console.log(balance)
+//console.log(score)
+//console.log(balance.toString().length)
+//console.log(typeof balance);
+//console.log(balance.toFixed(3));
+//const otherNumber = 123.9845
+//console.log(otherNumber.toPrecision(3));
+const Number = 123456;
+//console.log(Number.toLocaleString('en-IN'));
+//console.log(Number.toLocaleString("en-IN"));
+
+////////+++++++++++++++++++ MATHS +++++++++++++++////////////////////////
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.abs(4))
+// console.log(Math.round(4.45))
+// console.log(Math.ceil(4.45))
+// console.log(Math.floor(4.45))
+// console.log(Math.max(4,5,7,2,87,32))
+// console.log(Math.min(4,5,7,2,87,32))
+
+    // console.log(Math.random());
+    // console.log((Math.random()*10)+1);
+    // console.log(Math.floor(Math.random()*10)+1);
+ const min=1
+ const max=6
+console.log(Math.floor(Math.random()*(max-min+1)+min))
