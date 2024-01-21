16b1aecc45bd369825ccbd8f5d68dec0d7ffcd22
RamBindal <128307816+RamBindal@users.noreply.github.com>
2024-01-21 14:20:40 +0000

    datatypes summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,25 @@
+// Primitive
+// 7 Types - String,Number,Boolean,null,undefined,Symbol,BigInt
+
+// Non-Primitive (Reference)
+//Array,Objects,Functions
+
+//const id=Symbol("123")
+//const anotherId=Symbol("123")
+//console.log(typeof id);
+//console.log(typeof anotherId);
+//console.log(id===anotherId);
+
+//const bigNumber = 320975823852850235n
+//console.log(typeof bigNumber);
+//console.log(bigNumber);
+
+//const heroes = ["Shaktiman","naagraj","doga"] //Array
+//let myObj = {    //Objects
+    //name:"ram",
+    //age:22,
+//}
+//const myFunction = function(){   //Functions
+    //console.log("Hello World!!!")
+//}
+//myFunction()
