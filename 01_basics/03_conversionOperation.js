f6cd1ea3525419312dea2ccdf6dbd5dd755441f2
RamBindal <128307816+RamBindal@users.noreply.github.com>
2024-01-21 12:32:56 +0000

    converaionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = 33
+let scores="33a"
+let scoores=null
+let scures=undefined
+let scuures=true
+let scu0res="Ram"
+let scuores=1
+let cuores="1"
+let suores=""
+let sores=33
+
+//console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof scores);
+//console.log(typeof scoores);
+//console.log(typeof scures);
+//console.log(typeof scuures);
+//console.log(typeof scu0res);
+//console.log(typeof scuores);
+//console.log(typeof cuores);
+//console.log(typeof suores);
+console.log(typeof sores);
+
+//let valueInBool = Boolean(cuores)
+//let valueInBoolean = Boolean(suores)
+//console.log(typeof valueInBoolean);
+//console.log(typeof valueInBool);
+//console.log( valueInBoolean);
+//console.log( valueInBool);
+let valueInString = String(sores)
+console.log(typeof valueInString);
+console.log( valueInString);
+
