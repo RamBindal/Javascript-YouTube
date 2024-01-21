4f65d7d4aadc0a3aab2d370a883240523103a10e
RamBindal <128307816+RamBindal@users.noreply.github.com>
2024-01-21 07:20:06 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144556
+let accountEmail="ram@gmail.com"
+var accountPassword="12345"
+accountCity="Shamli"
+
+//accountId=10//not allowed
+//console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
