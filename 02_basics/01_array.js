3181b6fc8da02b86bd12aee8ede497e11af8c959
RamBindal <128307816+RamBindal@users.noreply.github.com>
2024-01-22 08:34:50 +0000

    array1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,32 @@
+//// ARRAY ////
+
+ const myArr = [0,1,2,3,4,5]
+// const myHeroes = ["Thor","Captain America","Iron Man","SuperMan","Hulk"]
+// const myArr1 = new Array(1,2,3,4)
+// console.log(myArr);
+// console.log(myArr[0]);
+// console.log(myHeroes);
+
+// /// Array Methods ///
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(10)
+// myArr.shift()
+//myArr.includes(10)  //Gives False
+//console.log(myArr.indexOf(1))
+
+// const newArr = myArr.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+
+////// SCLICE OR SPLICE /////
+console.log("A",myArr)
+const Arr =myArr.slice(1,4)
+console.log(Arr)
+console.log("B",myArr)
+const Arr2 = myArr.splice(1,4)
+console.log(Arr2)
+console.log("C",myArr)
+
+
+ 
