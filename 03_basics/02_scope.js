653689957edefab560a3892f91dcbc7e0b2d3866
RamBindal <128307816+RamBindal@users.noreply.github.com>
2024-01-25 18:43:06 +0000

    scope level and mini hoisting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -12,4 +12,36 @@ if (true) {
 }
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+function one() {
+  const username = "Ram Bindal";
+  function two() {
+    const website = "You Tube";
+    console.log(username);
+  }
+  two();
+  // console.log(website);
+}
+// one();
+
+//+++++++++++++++++++++++++++Interesting+++++++++++++++++++++++++++//
+// function addone(num) {
+//   return num + 1;
+// }
+// addone(5);
+
+// const addtwo = function (num) {
+//   return num + 1;
+// };
+// addtwo(5);
+
+console.log(addone(5)); //Ok With It
+function addone(num) {
+  return num + 1;
+}
+
+// console.log(addtwo(5)); //Gives An Error  //Function Hoisting
+const addtwo = function (num) {
+  return num + 1;
+};
